f53f866b17a9487e84a3d7c3667fc52fec58fa16
Abhijith Darshan Ravindra <abhijith.darshan@hotmail.com>
2018-01-21 11:26:53 +0100

    Service, Http Get and models

---
+++ src/app/ad.model.ts
@@ -0,0 +1,14 @@
+export class AdModel {
+  constructor(
+    public url:string,
+    public title:string,
+    public stadt: string,
+    public plz: number,
+    public zimmer: number,
+    public space: number,
+    public visibility: boolean,
+    public purpose: string,
+    public sellPrice?: number,
+    public baseRent?: number
+    ){}
+}
---
+++ src/app/services/advertisements.service.ts
@@ -0,0 +1,43 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpRequest} from '@angular/common/http';
+import 'rxjs/Rx';
+import {Observable} from "rxjs/Observable";
+import {AdModel} from '../ad.model';
+
+@Injectable()
+export class AdvertisementsService {
+
+  public advertisements: AdModel[] = [];
+
+  constructor(private http: HttpClient) {
+    console.log('Hello AdService!');
+  }
+
+  getAdvertisements()
+  {
+    return this.http.get('/v1/advertisements')
+      .map((response: any) => {
+        const tenAds = response.data.splice(0, 10);
+        console.log(tenAds);
+        for(const oneAd of tenAds)
+        {
+          this.advertisements.push(
+            new AdModel(
+              oneAd.advertisementAssets["0"].advertisementThumbnails.inventory_m.url,
+              oneAd.title,
+              oneAd.realestateSummary.address.city,
+              oneAd.realestateSummary.address.postalCode,
+              oneAd.realestateSummary.numberOfRooms,
+              oneAd.realestateSummary.space,
+              oneAd.userWishes.visibleAddress,
+              oneAd.purpose,
+              oneAd.advertisementPrice.sellPrice,
+              oneAd.advertisementPrice.baseRent
+              )
+          );
+        }
+        return this.advertisements;
+      })
+      .catch((error) => Observable.throw(error));
+  }
+}
